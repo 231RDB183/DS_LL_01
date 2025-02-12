3079f70ea88fea3ffa4b511ba6bf96d6a84a0edc
Kārkliņš Deivids 231RDB183 <deivids.karklins@edu.rtu.lv>
2025-02-12 15:31:55 +0000

    LD1

--- LL_1.py
+++ LL_1.py
@@ -24,6 +24,15 @@ class LinkedList:
 
     # WRITE FIND_MIDDLE_NODE METHOD HERE #
     #                                    #
+    def find_middle_node(self):
+        slow = self.head ##pt
+        fast = self.head ##pt
+        
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
     #                                    #
     #                                    #
     #                                    #
@@ -36,6 +45,8 @@ my_linked_list.append(2)
 my_linked_list.append(3)
 my_linked_list.append(4)
 my_linked_list.append(5)
+my_linked_list.append(5)
+
 
 print( my_linked_list.find_middle_node().value )
 
--- LL_2.py
+++ LL_2.py
@@ -24,6 +24,15 @@ class LinkedList:
     # WRITE HAS_LOOP METHOD HERE #
     #                            #
     #                            #
+    def has_loop(self):
+        slow = self.head ##pt
+        fast = self.head ##pt
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
     #                            #
     #                            #
     ##############################
@@ -35,6 +44,8 @@ my_linked_list_1 = LinkedList(1)
 my_linked_list_1.append(2)
 my_linked_list_1.append(3)
 my_linked_list_1.append(4)
+my_linked_list_1.append(4)
+
 my_linked_list_1.tail.next = my_linked_list_1.head
 print(my_linked_list_1.has_loop() ) # Returns True
 
@@ -45,6 +56,8 @@ my_linked_list_2 = LinkedList(1)
 my_linked_list_2.append(2)
 my_linked_list_2.append(3)
 my_linked_list_2.append(4)
+my_linked_list_2.append(2)
+
 print(my_linked_list_2.has_loop() ) # Returns False
 
 
